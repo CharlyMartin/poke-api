ca415593612e177dd3d158623417123b42dffbb1
Charly Martin <charly@hey.com>
2023-06-20 16:16:23 +0100

    Install react-query and fetch pokemon list

--- src/components/pages/index.tsx
+++ src/components/pages/index.tsx
@@ -1,3 +1,44 @@
+import { PokeAPI } from "pokeapi-types";
+import { Link } from "react-router-dom";
+
+import { usePokemons } from "../../hooks/use-pokemons";
+
 export default function IndexPage() {
-  return <h1>List Pokemons</h1>;
+  const response = usePokemons();
+
+  return (
+    <Loader
+      {...response}
+      render={(data) => {
+        return (
+          <ul>
+            {data.map((pokemon) => {
+              const { name } = pokemon;
+              return (
+                <Link key={name} to={`/pokemon/${name}`}>
+                  <div>
+                    <h1>{name}</h1>
+                  </div>
+                </Link>
+              );
+            })}
+          </ul>
+        );
+      }}
+    />
+  );
+}
+
+type LoaderProps = ReturnType<typeof usePokemons> & {
+  render: (data: PokeAPI.NamedAPIResource[]) => React.ReactElement;
+};
+
+function Loader(props: LoaderProps) {
+  const { data, render, isLoading, isFetching, error } = props;
+
+  if (isLoading || isFetching) return <p>Loading...</p>;
+  if (error) return <p>Error</p>;
+  if (!data) return <p>No Data</p>;
+
+  return render(data.data.results);
 }
---
+++ src/hooks/use-pokemons.ts
@@ -0,0 +1,19 @@
+import { useQuery } from "@tanstack/react-query";
+import { useSearchParams } from "react-router-dom";
+
+import { getPokemons } from "../api";
+
+export function usePokemons() {
+  const [params] = useSearchParams();
+
+  const args = {
+    limit: params.get("limit") || "",
+    offset: params.get("offset") || "",
+  };
+
+  return useQuery({
+    queryKey: ["pokemons", args],
+    queryFn: () => getPokemons(args),
+    keepPreviousData: true,
+  });
+}
