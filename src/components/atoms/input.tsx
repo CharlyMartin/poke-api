aef0f0af5a2fd4008b34349b8102cb74da51fcef
Charly Martin <charly@hey.com>
2023-06-20 17:33:10 +0100

    fixup! Add input component

--- src/components/atoms/input.tsx
+++ src/components/atoms/input.tsx
@@ -1,14 +1,18 @@
 import React from "react";
 
 export default function TextInput(props: React.ComponentProps<"input">) {
-  const [value, setValue] = React.useState(props.defaultValue);
+  const { value: v, onChange } = props;
+  const [value, setValue] = React.useState(v);
 
   return (
     <input
       {...props}
       className="w-16 border-none bg-transparent p-2 focus:outline-none"
       value={value}
-      onChange={(e) => setValue(e.target.value)}
+      onChange={(e) => {
+        if (onChange) onChange(e);
+        setValue(e.target.value);
+      }}
     />
   );
 }
