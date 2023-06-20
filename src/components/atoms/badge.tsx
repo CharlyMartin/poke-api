59c1ff684a0e2ad7a694f5a99d78d4455b804518
Charly Martin <charly@hey.com>
2023-06-20 18:28:26 +0100

    Add variants to button and badge components

--- src/components/atoms/badge.tsx
+++ src/components/atoms/badge.tsx
@@ -1,12 +1,22 @@
+import cn from "classnames";
+
 type Props = {
   children: React.ReactNode;
+  variant?: "sm" | "md" | "lg";
 };
 
 export default function Badge(props: Props) {
-  const { children } = props;
+  const { children, variant = "sm" } = props;
 
   return (
-    <span className="rounded-full bg-slate-200 px-2.5 py-0.5 text-sm font-medium">
+    <span
+      className={cn(
+        "rounded-full bg-slate-200 px-2.5 py-0.5 text-sm font-medium",
+        variant == "sm" && "px-2.5 py-0.5 text-sm font-medium",
+        variant == "md" && "px-3 py-0.5 text-base font-medium",
+        variant == "lg" && "px-4 py-1 text-lg font-medium"
+      )}
+    >
       {children}
     </span>
   );
--- src/components/atoms/button.tsx
+++ src/components/atoms/button.tsx
@@ -1,9 +1,15 @@
+import cn from "classnames";
+
 export default function Button(props: React.ComponentProps<"button">) {
+  const { className } = props;
   return (
     <button
       type="button"
       {...props}
-      className="rounded bg-slate-700 px-4 py-2 font-bold text-white hover:bg-slate-900"
+      className={cn(
+        "rounded bg-slate-700 px-4 py-2 font-bold text-white hover:bg-slate-900",
+        className
+      )}
     />
   );
 }
